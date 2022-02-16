9cbc8f201b9d3412f428a111bb12e2f47c0360ed
Gabriella <g4b.dev@gmail.com>
2022-02-16 13:27:34 -0300

    Add new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { StarHomeComponent } from './star-home/star-home.component';
+import { StarMovieComponent } from './star-movie/star-movie.component';
+import { StarPlanetsComponent } from './star-planets/star-planets.component';
+import { StarCharComponent } from './star-char/star-char.component';
+
 
 const routes: Routes = [
+  {path: '', component: StarHomeComponent},
   {path: 'home', component: StarHomeComponent},
-  {path: 'home', component: StarHomeComponent},
+  {path: 'movies', component: StarMovieComponent},
+  {path: 'characters', component: StarCharComponent},
+  {path: 'planets', component: StarPlanetsComponent},
 ];
  
 @NgModule({
---
+++ src/app/star-movie/star-movie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-star-movie',
+  templateUrl: './star-movie.component.html',
+  styleUrls: ['./star-movie.component.scss']
+})
+export class StarMovieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
